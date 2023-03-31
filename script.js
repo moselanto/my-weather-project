49ab11ad1b233590885df2268abee5c5fd3e92d6
MOSES MUNYI <50106345+moselanto@users.noreply.github.com>
2023-03-31 22:33:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,88 @@
- const apiKey = "ENTER YOUR API KEY";
+<script>
+    let appId = '71f6779186cc32448b4c412eea65b982';
+let units = 'metric'; 
+let searchMethod; // q means searching as a string.
 
-const main = document.getElementById('main');
-const form = document.getElementById('form');
-const search = document.getElementById('search');
-  
-const url = (city)=> `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+function getSearchMethod(searchTerm) {
+    if(searchTerm.length === 5 && Number.parseInt(searchTerm) + '' === searchTerm)
+        searchMethod = 'zip';
+    else 
+        searchMethod = 'q';
+}
 
+function searchWeather(searchTerm) {
+    getSearchMethod(searchTerm);
+    fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`)
+        .then((result) => {
+            return result.json();
+        }).then((res) => {
+            init(res);
+    });
+}
 
-async function getWeatherByLocation(city){
-     
-         const resp = await fetch(url(city), {
-             origin: "cros" });
-         const respData = await resp.json();
-     
-           addWeatherToPage(respData);
-          
-     }
+function init(resultFromServer) {
+    switch (resultFromServer.weather[0].main) {
+        case 'Clear':
+            document.body.style.backgroundImage = "url('clearPicture.jpg')";
+            break;
+        
+        case 'Clouds':
+            document.body.style.backgroundImage = "url('cloudyPicture.jpg')";
+            break;
 
-      function addWeatherToPage(data){
-          const temp = Ktoc(data.main.temp);
+        case 'Rain':
+        case 'Drizzle':
+            document.body.style.backgroundImage = "url('rainPicture.jpg')";
+            break;
 
-          const weather = document.createElement('div')
-          weather.classList.add('weather');
+        case 'Mist':
+            document.body.style.backgroundImage = "url('mistPicture.jpg')";
+            break;    
+        
+        case 'Thunderstorm':
+            document.body.style.backgroundImage = "url('stormPicture.jpg')";
+            break;
+        
+        case 'Snow':
+            document.body.style.backgroundImage = "url('snowPicture.jpg')";
+            break;
 
-          weather.innerHTML = `
-          <h2><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /> ${temp}°C <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></h2>
-          <small>${data.weather[0].main}</small>
-          
-          `;
+        default:
+            break;
+    }
 
+    let weatherDescriptionHeader = document.getElementById('weatherDescriptionHeader');
+    let temperatureElement = document.getElementById('temperature');
+    let humidityElement = document.getElementById('humidity');
+    let windSpeedElement = document.getElementById('windSpeed');
+    let cityHeader = document.getElementById('cityHeader');
 
-        //   cleanup 
-          main.innerHTML= "";
-           main.appendChild(weather);
-      };
+    let weatherIcon = document.getElementById('documentIconImg');
+    weatherIcon.src = 'http://openweathermap.org/img/w/' + resultFromServer.weather[0].icon + '.png';
 
+    let resultDescription = resultFromServer.weather[0].description;
+    weatherDescriptionHeader.innerText = resultDescription.charAt(0).toUpperCase() + resultDescription.slice(1);
+    temperatureElement.innerHTML = Math.floor(resultFromServer.main.temp) + '&#176;';
+    windSpeedElement.innerHTML = 'Wind Speed: ' + Math.floor(resultFromServer.wind.speed) + ' meter/s';
+    cityHeader.innerHTML = resultFromServer.name;
+    humidityElement.innerHTML = 'Humidity levels: ' + resultFromServer.main.humidity +  '%';
 
-     function Ktoc(K){
-         return Math.floor(K - 273.15);
-     }
+    setPositionForWeatherInfo();
+}
 
+function setPositionForWeatherInfo() {
+    let weatherContainer = document.getElementById('weatherContainer');
+    let weatherContainerHeight = weatherContainer.clientHeight;
+    let weatherContainerWidth = weatherContainer.clientWidth;
 
+    weatherContainer.style.left = `calc(50% - ${weatherContainerWidth/2}px)`;
+    weatherContainer.style.top = `calc(50% - ${weatherContainerHeight/1.3}px)`;
+    weatherContainer.style.visibility = 'visible';
+}
 
-     form.addEventListener('submit',(e) =>{
-        e.preventDefault();
-
-        const city = search.value;
-
-        if(city){
-            getWeatherByLocation(city)
-        }
-
-     });
+document.getElementById('searchBtn').addEventListener('click', () => {
+    let searchTerm = document.getElementById('searchInput').value;
+    if(searchTerm)
+        searchWeather(searchTerm);
+});
+</script>
